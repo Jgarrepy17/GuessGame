1ca2aa747c65f9648ddfb5decbed235b71f4344f
Jgarrepy17 <jgarrepy@pathfinderstudents.org>
2017-09-20 13:37:27 -0400

    Add files via upload

---
+++ Guessing Game.js
@@ -0,0 +1,35 @@
+/* Guessing Game by Jake Garrepy */
+var totalTurns = 0;
+var Games = 0;
+var Average = 0;
+var playAgain = "y";
+// var (totalTurns = 0;var Games = 0;var Average = 0;var playAgain = "y";)
+
+
+
+
+/*Start playing*/
+
+do{
+	var Answer = Math.floor(Math.random()*(99) + 1);
+	alert(Answer);
+	do	{
+		Guess =  prompt('Enter a guess 1-100');
+		do	{
+			Turns = (0);
+		}
+		if (Guess > Answer){
+		alert('too high');
+	}
+		else if(Guess < Answer){
+		alert('too low');
+		}
+		Turns ++;
+		alert(Turns +'Turns')
+	}
+	while (Guess != Answer);
+		alert('congratulations');
+	playAgain = prompt("Would you like to play again? (y = Yes)")
+}
+while (playAgain == 'y')
+/* End Playing*/
